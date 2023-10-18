df4078fdd782621f3b15fefac030f7828deed01d
maxigeist <geist.maximo@gmail.com>
2023-10-18 10:23:27 -0300

    feat: add api routes

---
+++ src/service/apis.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+
+const apiInstance = axios.create({
+    baseURL: 'http://localhost:8000/api',
+})
+
+
+export const getProducts = async () => {
+    try{
+        const response = await apiInstance.get('/products');
+        return response.data;
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+export const getStatus = async () => {
+    try{
+        const response = await apiInstance.get('/status');
+        return response.data;
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+export const getEarnings = async () => {
+    try{
+        const response = await apiInstance.get('/earnings');
+        return response.data;
+    }
+    catch (error){
+        console.log(error)
+    }
+
+}
+
+export const getCredit = async () => {
+    try{
+        const response = await apiInstance.get('/credit');
+        return response.data;
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+
+
+
