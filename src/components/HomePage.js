e204ab6006024c2cffa0aa5a876ef87ff9498071
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2023-10-18 12:10:48 -0300

    ui update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,41 @@
+import { useState, useEffect } from 'react';
 import { getCredit, getEarnings, getProducts, getStatus, getMachines } from '../service/apis';
 import MachineStatus from './MachineStatus';
 
 function HomePage() {
-  const machine = {
-    machineId: getMachines(),
-    products: getProducts(),
-    status: getStatus(),
-    earnings: getEarnings(),
-    credit: getCredit()
+
+  const getMachine = (machineId) => {
+    return {
+      machineId: machineId,
+      products: getProducts(machineId),
+      status: getStatus(machineId),
+      earnings: getEarnings(machineId),
+      credit: getCredit(machineId)
+    }
   }
   
-  c
+  const [machines, setMachines] = useState([1, 2, 3, ,4, 5, 6]);
+  
+  useEffect(() => {
+    // getMachines().then((response) => setMachines(response))
+}, []);
+
+  
+  getMachines();
+  const [currentMachine, setCurrentMachine] = useState();
+
+  const handleCurrentMachine = (machineId) => setCurrentMachine(getMachine(machineId));
 
   return (
     <div>
-      <MachineStatus machine={machine}/>
+      <div>
+        {machines?.map((machine) => (
+        <button>
+          {machine}
+        </button>
+        ))}
+      </div>
+      <MachineStatus machine={currentMachine}/>
     </div>
   );
 }
